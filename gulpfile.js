feacc5fe3c809d11af2d2b54792a33add72cdfda
1045 <1045@student.itgymnazium.cz>
2022-10-30 11:12:50 +0100

    Gulpfile(update);package(update);htaccess(update)

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,15 @@
 var { src, dest, watch, series } = require("gulp"),
     sass = require("gulp-sass")(require("sass")),
     autoprefixer = require("gulp-autoprefixer"),
+    imagemin = require("gulp-imagemin"),
     cssnano = require("gulp-cssnano"),
     uglify = require("gulp-uglify"),
     rename = require("gulp-rename"),
-    concat = require("gulp-concat");
+    concat = require("gulp-concat"),
+    cache = require("gulp-cache");
 
 
-function scss()
+const scss = () =>
 {
     return src("src/styles/scss/*.scss")
     .pipe(sass())
@@ -16,34 +18,52 @@ function scss()
     .pipe(rename({suffix: ".min"}))
     .pipe(dest("src/styles"));
 }
-function styles()
+const styles = () =>
 {
     return src(["src/styles/main.min.css"])
         .pipe(concat("main.css"))
         .pipe(rename({suffix: ".min"}))
         .pipe(cssnano())
-        .pipe(dest("dist/css"));
+        .pipe(dest("www/dist/css"));
 }
 
-function scripts()
+const scripts = () =>
 {
     return src("src/scripts/js/*.js")
         .pipe(concat("main.js"))
         .pipe(rename({suffix: ".min"}))
         .pipe(uglify())
-        .pipe(dest("dist/js"));
+        .pipe(dest("www/dist/js"));
 }
 
-function watchTask()
+const fonts = () =>
+{
+    return src("assets/fonts/*")
+        .pipe(dest("www/dist/assets/fonts"));
+}
+
+const images = () => 
+{
+    return src("assets/images/*")
+        .pipe(cache(imagemin({optimizationLevel: 3, progressive: true, interlaced: true})))
+        .pipe(dest("www/dist/assets/images"));
+}
+
+const watchTask = () =>
 {
     watch("src/styles/scss/*.scss", scss);
     watch("src/styles/", styles);
-    watch("src/scripts/js/*.js", scripts)
+    watch("src/scripts/js/*.js", scripts);
+    watch("assets/fonts/*.ttf", fonts);
+    watch("assets/images/*", images)
 }
 
-exports.default = series(
+exports.default = series
+(
     scss,
+    fonts,
     styles,
+    images,
     scripts,
-    watchTask
+    watchTask,
 );
